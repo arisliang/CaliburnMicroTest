9501aed070636e8cdc32359969441dfdb801fb92
Aris Liang <arisliang@navig8group.com>
2015-04-15 15:11:04 +0800

    install caliburn.micro from nuget. adds first view, view model, bootstrapper

---
+++ CaliburnMicroTest.WpfUi/AppBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using CaliburnMicroTest.WpfUi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnMicroTest.WpfUi
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+
+            DisplayRootViewFor<AppViewModel>();
+        }
+    }
+}
